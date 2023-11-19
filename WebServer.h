0d1a67439a5b75f04dc43947c882c69d7bd8ed21
charles <cgiguere@iname.com>
2023-11-19 06:15:16 -0500

    initial

---
+++ WebServer.h
@@ -0,0 +1,358 @@
+// Web server Functions
+//
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <Update.h>
+#include <esp32-hal-adc.h>
+#include "secrets.h"
+
+WebServer server(80);
+
+// Web server host name
+const char *hostName = "ai_draw";
+
+const char* getCSS() {
+    return 
+"<style>\
+                          .loading {\
+                            background-color: #f3f3f3;\
+                            cursor: not-allowed;\
+                          }\
+                          .spinner {\
+                            display: inline-block;\
+                            width: 40px;\
+                            height: 40px;\
+                            border: 5px solid rgba(0,0,0,.2);\
+                            border-top-color: #008000;\
+                            border-radius: 100%;\
+                            animation: spin 1s linear infinite;\
+                          }\
+                          @keyframes spin {\
+                            to { transform: rotate(360deg); }\
+                          }\
+                          .column {\
+                            width: 50%;\
+                            padding: 10px;\
+                            box-sizing: border-box;\
+                          }\
+                          .separator {\
+                            width: 2px;\
+                            background: #ddd;\
+                            height: 100%;\
+                            position: absolute;\
+                            left: 50%;\
+                            top: 0;\
+                          }\
+                          .left { float: left; }\
+                          .right { float: right; }\
+                          h1 { text-align: center; }\
+                    .label {\
+                      font-weight: bold;\
+                      font-size: 25px;\
+                    }\
+                    input[type=\"text\"] {\
+                      background-color: #f3f3f3;\
+                      border: 1px solid #ccc;\
+                      padding: 10px;\
+                      width: 50%;\
+                      box-sizing: border-box;\
+                      font-size: 25px;\
+                    }\
+                    input[type=\"submit\"] {\
+                      background-color: #008800;\
+                      color: white;\
+                      border: none;\
+                      padding: 10px;\
+                      text-transform: uppercase;\
+                      cursor: pointer;\
+                      font-size: 25px;\
+                      margin-top: 10px;\
+                      width: 50%;\
+                    }\
+                    input[type=\"submit\"]:hover {\
+                      background-color: #006600;\
+                    }\
+                          button {\
+                            font-size: 40px;\
+                            padding: 15px;\
+                            width: 90%;\
+                            box-sizing: border-box;\
+                            margin: 20px 5%;\
+                            border-radius: 25px;\
+                            transition: background-color 0.3s, transform 0.3s;\
+                          }\
+                          button:hover {\
+                            background-color: #ddd;\
+                          }\
+                          button:active {\
+                            transform: scale(0.95);\
+                          }\
+                          </style>";
+};
+
+const char* getHTMLPageUpdate() {
+    return 
+"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+    "<style>"
+    "body {"
+    "    font-size: 40px;" /* Change the size as needed */
+    "}"
+    "input[type=submit], input[type=file], #homeButton {"
+    "    font-size: 40px;"    /* Change the size as needed */
+    "    padding: 10px 20px;" /* Change the padding as needed */
+    "}"
+    "progress {"
+    "    width: 100%;"
+    "    height: 100px;" /* Increase the height as needed */
+    "}"
+    "#successMessage {"
+    "    display: none;" /* Initially hidden */
+    "    color: green;"
+    "}"
+    "</style>"
+    "<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+    "<input type='file' name='update'>"
+    "<input type='submit' value='Update'>"
+    "</form>"
+    "<progress id='prg' value='0' max='100'></progress>"
+    "<div id='successMessage'>"
+    "Upload completed successfully!"
+    "<br>"
+    "<button id='homeButton' onclick='window.location=\"/\"'>Return to Home</button>"
+    "</div>"
+    "<script>"
+    "$('form').submit(function(e){"
+    "    e.preventDefault();"
+    "    var form = $('#upload_form')[0];"
+    "    var data = new FormData(form);"
+    "    $.ajax({"
+    "        url: '/upload',"
+    "        type: 'POST',"
+    "        data: data,"
+    "        contentType: false,"
+    "        processData:false,"
+    "        xhr: function() {"
+    "            var xhr = new window.XMLHttpRequest();"
+    "            xhr.upload.addEventListener('progress', function(evt) {"
+    "                if (evt.lengthComputable) {"
+    "                    var per = evt.loaded / evt.total;"
+    "                    $('#prg').val(Math.round(per*100));"
+    "                }"
+    "            }, false);"
+    "            return xhr;"
+    "        },"
+    "        success:function(d, s) {"
+    "            $('#successMessage').show();" // Show the success message and button
+    "        },"
+    "        error: function (a, b, c) {"
+    "        }"
+    "    });"
+    "});"
+    "</script>";
+}
+
+// Structure to store commands available on the web application
+struct Command
+{
+    const char *name;
+    const char *endpoint;
+    void (*handler)();
+};
+
+// List of Commands available on the web application
+void handleHello(void);
+void handleUpdate(void);
+
+// Fetch commands name, route and handling function to be called
+Command fetchCommands[] = {
+    {"Home", "/", handleHello},
+};
+
+// Post commands name, route and handling function to be called
+Command postCommands[] = {
+    {"Upload code", "/update", handleUpdate},
+
+};
+
+// Functions to handle post data by the forms on the web application
+const char *uploadEndpoint = "/upload";
+
+// Build a list of commands available to display on the web application
+String commandsList(void)
+{
+    String commandList = "<div class='separator'></div>\
+                          <div class='column left'>\
+                          <h1>Commands</h1>";
+    for (Command &cmd : fetchCommands)
+    {
+        if (cmd.endpoint == "/update")
+        {
+            commandList += "<button style='font-size: 40px; padding: 15px; width: 90%; box-sizing: border-box; margin: 20px 5%; border-radius: 25px;' onclick=\"location.href='";
+            commandList += cmd.endpoint;
+            commandList += "'\">";
+        }
+        else
+        {
+            commandList += "<button id='";
+            commandList += cmd.endpoint;
+            commandList += "' style='font-size: 40px; padding: 15px; width: 90%; box-sizing: border-box; margin: 20px 5%; border-radius: 25px;' onclick='this.classList.add(\"loading\"); this.innerHTML=\"<div class=spinner></div>\"; fetch(\"";
+            commandList += cmd.endpoint;
+            commandList += "\").then(() => { this.classList.remove(\"loading\"); this.textContent=\"";
+            commandList += cmd.name;
+            commandList += "\"; })'>";
+        }
+
+        commandList += cmd.name;
+        commandList += "</button><br/>";
+    }
+    commandList += "</div>\
+                    <div class='column right'>\
+                    <h1>Settings Configuration</h1>";
+    for (Command &cmd : postCommands)
+    {
+
+        commandList += "<button style='font-size: 40px; padding: 15px; width: 90%; box-sizing: border-box; margin: 20px 5%; border-radius: 25px;' onclick=\"location.href='";
+        commandList += cmd.endpoint;
+        commandList += "'\">";
+
+        commandList += cmd.name;
+        commandList += "</button><br/>";
+    }
+    commandList += "</div>";
+
+    return commandList;
+}
+
+void sendPlainText(const char *fmt, ...)
+{
+    char buf[256]; // Buffer to hold the formatted string. Adjust the size as needed.
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(buf, sizeof(buf), fmt, args);
+    va_end(args);
+
+    server.send(200, "text/plain", buf);
+}
+
+void handleHello(void)
+{
+    String body = "<div style='border: 2px solid #008800; background-color: #e6f7e6; padding: 10px; border-radius: 10px; margin: 10px; font-size: 24px;'>\
+                   URL: " +
+                  String(hostName) + ".local <br/>" +
+                  "IP address: " + WiFi.localIP().toString() + "<br/>" +
+                  "Connected to: " + String(ssid) + "<br/>" +
+                  "ESP32 Temperature: " + String(static_cast<long>(temperatureRead())) + " C" +
+                  "</div>"
+                  + getCSS()
+                  + commandsList();
+    server.send(200, "text/html", body);
+}
+
+void handleUpdate(void)
+{
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", getHTMLPageUpdate());
+}
+
+void handleNotFound()
+{
+    String message = "Command Not Found\n\n";
+    message += "URI: ";
+    message += server.uri();
+    message += "\nMethod: ";
+    message += (server.method() == HTTP_GET) ? "GET" : "POST";
+    message += "\nArguments: ";
+    message += server.args();
+    message += "\n";
+    for (uint8_t i = 0; i < server.args(); i++)
+    {
+        message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+    }
+    server.send(404, "text/plain", message);
+}
+
+// Setup of routes for handling requests coming from the web application
+void setupCommands(void)
+{
+    // Register the command handlers with the web server
+    for (Command &cmd : fetchCommands)
+    {
+        server.on(cmd.endpoint, HTTP_GET, cmd.handler);
+    }
+    for (Command &cmd : postCommands)
+    {
+        server.on(cmd.endpoint, HTTP_GET, cmd.handler);
+    }
+
+    server.onNotFound(handleNotFound);
+
+    /*handling uploading firmware file */
+    server.on(
+        uploadEndpoint, HTTP_POST, []()
+        {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart(); },
+        []()
+        {
+            HTTPUpload &upload = server.upload();
+            if (upload.status == UPLOAD_FILE_START)
+            {
+                Serial.printf("Update: %s\n", upload.filename.c_str());
+                if (!Update.begin(UPDATE_SIZE_UNKNOWN))
+                { // start with max available size
+                    Update.printError(Serial);
+                }
+            }
+            else if (upload.status == UPLOAD_FILE_WRITE)
+            {
+                /* flashing firmware to ESP*/
+                if (Update.write(upload.buf, upload.currentSize) != upload.currentSize)
+                {
+                    Update.printError(Serial);
+                }
+            }
+            else if (upload.status == UPLOAD_FILE_END)
+            {
+                if (Update.end(true))
+                { // true to set the size to the current progress
+                    Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+                }
+                else
+                {
+                    Update.printError(Serial);
+                }
+            }
+        });
+}
+
+void initWebServer()
+{
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.println("");
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(100);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    if (MDNS.begin(hostName))
+    {
+        Serial.println("MDNS responder started");
+    }
+
+    setupCommands();
+
+    server.begin();
+    Serial.println("HTTP server started");
+}
---
+++ base64images.ino
@@ -0,0 +1,125 @@
+#include <PNGdec.h>
+#include <TFT_eSPI.h>
+#include "arduino_base64.hpp"
+
+#include "mandalaBase64Png.h"
+
+#define MAX_IMAGE_WIDTH 240 // Adjust for your images
+
+int16_t xpos = 0;
+int16_t ypos = 0;
+
+PNG png; // PNG decoder instance
+TFT_eSPI tft = TFT_eSPI();
+
+uint8_t output[50000L];
+// uint8_t* output;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    tft.begin();
+    // tft.setRotation(2);
+    tft.fillScreen(TFT_WHITE);
+
+    // output = (uint8_t*) ps_malloc(50000L);
+
+    // if (output == NULL) {
+    //     Serial.println("Failed to allocate memory in PSRAM");
+    //     return;
+    // }
+    
+    size_t length = base64::decodeLength(mandalaBase64Png);
+    base64::decode(mandalaBase64Png, output);
+
+    Serial.printf("base64 decoded length = %ld\n", length);
+
+    // displayPngFromRam(panda,sizeof(panda));
+    displayPngFromRam(output, length);
+    // Serial.println((const char *)output);
+}
+
+void loop()
+{
+    delay(1);
+}
+
+void displayPngFromRam(const unsigned char *pngImageinC, size_t length)
+{
+    int res = png.openRAM((uint8_t *)pngImageinC, length, pngDraw);
+    if (res == PNG_SUCCESS)
+    {
+        Serial.println("Successfully opened png file");
+        Serial.printf("image specs: (%d x %d), %d bpp, pixel type: %d\n", png.getWidth(), png.getHeight(), png.getBpp(), png.getPixelType());
+        Serial.printf("Image size: %d\n", length);
+        Serial.printf("Buffer size: %d\n", png.getBufferSize());
+        tft.startWrite();
+        uint32_t dt = millis();
+        res = png.decode(NULL, 0);
+        Serial.print(millis() - dt);
+        Serial.println("ms");
+        tft.endWrite();
+        // png.close(); // not needed for memory->memory decode
+    }
+    else
+    {
+        printPngError(res);
+    }
+}
+
+//=========================================v==========================================
+//                                      pngDraw
+//====================================================================================
+// This next function will be called during decoding of the png file to
+// render each image line to the TFT.  If you use a different TFT library
+// you will need to adapt this function to suit.
+// Callback function to draw pixels to the display
+void pngDraw(PNGDRAW *pDraw)
+{
+    uint16_t lineBuffer[MAX_IMAGE_WIDTH];
+    png.getLineAsRGB565(pDraw, lineBuffer, PNG_RGB565_BIG_ENDIAN, 0xffffffff);
+    // png.getLineAsRGB565(pDraw, lineBuffer, PNG_RGB565_BIG_ENDIAN, -1);
+    tft.pushImage(xpos, ypos + pDraw->y, pDraw->iWidth, 1, lineBuffer);
+
+    // // Print the buffer contents
+    // for (int i = 0; i < MAX_IMAGE_WIDTH; i++)
+    // {
+    //     Serial.printf("%d", lineBuffer[i]);
+    // }
+    // Serial.println();
+}
+
+void printPngError(int errorCode)
+{
+    switch (errorCode)
+    {
+    case PNG_SUCCESS:
+        Serial.println("PNG Success!");
+        break;
+    case PNG_INVALID_PARAMETER:
+        Serial.println("PNG Error Invalid Parameter");
+        break;
+    case PNG_DECODE_ERROR:
+        Serial.println("PNG Error Decode");
+        break;
+    case PNG_MEM_ERROR:
+        Serial.println("PNG Error Memory");
+        break;
+    case PNG_NO_BUFFER:
+        Serial.println("PNG Error No Buffer");
+        break;
+    case PNG_UNSUPPORTED_FEATURE:
+        Serial.println("PNG Error Unsupported Feature");
+        break;
+    case PNG_INVALID_FILE:
+        Serial.println("PNG Error Invalid File");
+        break;
+    case PNG_TOO_BIG:
+        Serial.println("PNG Error Too Big");
+        break;
+    default:
+        Serial.println("PNG Error Unknown");
+        break;
+    }
+}
