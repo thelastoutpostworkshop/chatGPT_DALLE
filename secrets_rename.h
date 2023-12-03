f242fdb2efa4a1151296ad3d1e98f5aae3efc5e3
charles <cgiguere@iname.com>
2023-12-03 11:08:53 -0500

    secrets instructions

---
+++ secrets_rename.h
@@ -0,0 +1,6 @@
+// Rename this file secrets.h
+// Enter your Wifi and OpenAI API key below
+//
+const char *ssid = "Enter here the SSID of your Wifi Network";
+const char *password = "Enter here the password of your Wifi Network";
+const char *chatGPT_APIKey = "Enter here your OpenAI API key";
